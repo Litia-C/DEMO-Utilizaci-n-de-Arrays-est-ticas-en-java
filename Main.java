255bb82a1757f8344a0eafc524a0ecd5152202ab
Maria <maria.montobbio@gracia.lasalle.cat>
2025-12-05 13:46:26 +0100

    menu inicial

---
+++ Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Bienvenido al selector de series\n");
+        System.out.println("¿Que quieres hacer?");
+        System.out.println("1. Ver lista de series");
+        System.out.println("2. Añadir series");
+        System.out.println("3. Quitar series");
+        System.out.println("4. Salir del programa");
+    }
+}
