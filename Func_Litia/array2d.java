cbde30baa7d4dd08d950b5e6e7e00aceffd4149c
Litia-C <litia.calderon@gracia.lasalle.cat>
2025-12-07 11:28:46 +0100

    Funcion mostrar en progreso

---
+++ Func_Litia/array2d.java
@@ -0,0 +1,18 @@
+public class array2d{
+
+    String[][] series = new String[1][1];
+
+
+    public static String mostrar(String[][] series){
+        
+        for (int i = 0; i < series.lenght; i++){
+            for (int j = 0; j < series[i].lenght; j++){
+                System.out.print(series[i][j]);
+            }
+            System.out.println();
+        }
+    }
+    
+    public static
+
+}
--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 
 public class Main {
+    String[][] series = new String[1][1];
     public static void main(String[] args) {
         System.out.println("Bienvenido al selector de series\n");
         System.out.println("¿Que quieres hacer?");
@@ -9,4 +10,16 @@ public class Main {
         System.out.println("3. Quitar series");
         System.out.println("4. Salir del programa");
     }
+
+
+    public static String mostrar(String[][] series){
+        
+        for (int i = 0; i < series.lenght; i++){
+            for (int j = 0; j < series[i].lenght; j++){
+                System.out.print(series[i][j]);
+            }
+            System.out.println();
+        }
+    }
+
 }
